b88c8cb836884c945b0297aa7f4d50a423558abc
Lucas <lucashnutn@gmail.com>
2019-11-15 21:50:57 -0300

    Binomial falta la v y e

--- Binomial.py
+++ Binomial.py
@@ -1,3 +1,14 @@
+#paneles
+import tkinter as tk
+from tkinter import simpledialog
+
+application_window = tk.Tk()
+application_window.withdraw()
+
+
+#tabla
+from tabulate import tabulate
+
 # importando modulos necesarios
 #%matplotlib inline
 
@@ -13,15 +24,41 @@ np.random.seed(2016) # replicar random
 #sns.set_context(rc={"figure.figsize": (8, 4)})
 
 
+
 # Graficando Binomial
-N, p = 20, 0.3 # parametros de forma
-binomial = stats.binom(N, p) # Distribución
+#n, p = 20, 0.3 # parametros de forma
+
+#print("Distribucion Binomial\n")
+
+#print("Ingrese el valor de n: ")
+#n=int(input())
+# print("Ingrese el valor de p: ")
+# p=float(input())
+
+
+n=int(simpledialog.askstring("Distribucion Binomial", "Ingrese el valor de n: ",
+                                parent=application_window))
+
+p=float(simpledialog.askstring("Distribucion Binomial", "Ingrese el valor de p: ",
+                                parent=application_window))
+
+
+
+binomial = stats.binom(n, p) # Distribución
+
+
+
 x = np.arange(binomial.ppf(0.01),
               binomial.ppf(0.99))
+
 fmp = binomial.pmf(x) # Función de Masa de Probabilidad
+
 plt.plot(x, fmp, '--')
 plt.vlines(x, 0, fmp, colors='b', lw=5, alpha=0.5)
 plt.title('Distribución Binomial')
 plt.ylabel('probabilidad')
 plt.xlabel('valores')
+
+print(tabulate({"x": x,"p": fmp}, headers="keys"))
+
 plt.show()
