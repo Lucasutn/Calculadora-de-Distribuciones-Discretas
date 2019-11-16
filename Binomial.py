7e41ea036c3ae2d55537c2167e3d435946cd7774
Lucas <lucashnutn@gmail.com>
2019-11-15 22:37:29 -0300

    Varianza y Media agregado

--- Binomial.py
+++ Binomial.py
@@ -48,10 +48,16 @@ binomial = stats.binom(n, p) # Distribución
 
 
 
-x = np.arange(binomial.ppf(0.01),
+x = np.arange(binomial.ppf(0.0001),
               binomial.ppf(0.99))
 
+Vx = np.arange(binomial.ppf(0),
+              binomial.ppf(1))
+Ex=binomial.pmf(Vx)
+
 fmp = binomial.pmf(x) # Función de Masa de Probabilidad
+
+
 
 plt.plot(x, fmp, '--')
 plt.vlines(x, 0, fmp, colors='b', lw=5, alpha=0.5)
@@ -59,6 +65,14 @@ plt.title('Distribución Binomial')
 plt.ylabel('probabilidad')
 plt.xlabel('valores')
 
+print("V(x)= " + str((np.var(Ex)*1000)) )
+print("E(x)= " + str((np.median(Ex)*1000)) )
+
+
 print(tabulate({"x": x,"p": fmp}, headers="keys"))
 
 plt.show()
+
+# x = np.arange(binomial.ppf(0.0),
+#               binomial.ppf(1))
+
